de1968aff25d26fa74be8c091f9b7011223773d8
vrsss <65066856+vrsss@users.noreply.github.com>
2021-01-20 20:38:58 +0200

    add html
    fdg

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter()
+    return render(request, 'blog/index.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
